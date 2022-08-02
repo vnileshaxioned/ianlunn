0c562dfcf5cfc3686d8694848deb09e0197d40c2
Nilesh Vishwakarma <nileshv@axioned.com>
2022-08-02 18:42:31 +0530

    project section struction completed

---
+++ wp-content/themes/ianlunn/template-parts/pages/portfolio/content-project.php
@@ -0,0 +1,23 @@
+<?php
+  $projects = get_sub_field('select_project');
+  if ($projects) { ?>
+  <section class="portfolio-project">
+    <div class="wrapper">
+      <ul class="portfolio-project__container">
+        <?php
+          foreach ($projects as $project) {
+            $post_id = $project->ID;
+            $title = $project->post_title;
+            $image = get_the_post_thumbnail_url($post_id);
+            if ($title || $image) { ?>
+            <li class="portfolio-project__list">
+              <figure class="portfolio-project__image">
+                <img src="<?php echo $image; ?>" alt="<?php echo $title; ?>">
+              </figure>
+            </li>
+          <?php }
+          } ?>
+        </ul>
+      </div>
+    </section>
+  <?php } ?>
